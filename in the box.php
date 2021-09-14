7352f247779770795bf00260b412760bfbaed4c9
vanhiptv2 <90610937+vanhiptv2@users.noreply.github.com>
2021-09-14 11:01:14 +0700

    Create in the box.php

---
+++ in the box.php
@@ -0,0 +1,8 @@
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=1128000,NAME="360p",CODECS="avc1.77.30,mp4a.40.2",RESOLUTION=640x360
+https://106bec9d8.vws.vegacdn.vn/cliptv_live/amlst:intheboxhd/chunklist_b1128000.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=2128000,NAME="720p",CODECS="avc1.77.31,mp4a.40.2",RESOLUTION=1280x720
+https://106bec9d8.vws.vegacdn.vn/cliptv_live/amlst:intheboxhd/chunklist_b2128000.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=3128000,NAME="1080p",CODECS="avc1.100.40,mp4a.40.2",RESOLUTION=1920x1080
+https://106bec9d8.vws.vegacdn.vn/cliptv_live/amlst:intheboxhd/chunklist_b3128000.m3u8
